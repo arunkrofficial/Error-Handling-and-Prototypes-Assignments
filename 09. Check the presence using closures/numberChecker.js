ea116571fb72d3689a1bd63e62decdece5115e04
Arun Kumar <iamrun289@gmail.com>
2023-04-27 08:40:41 +0530

    getNumber

---
+++ 09. Check the presence using closures/numberChecker.js
@@ -0,0 +1,10 @@
+function numberChecker(arr) {
+  return function(num) {
+    return arr.includes(num);
+  };
+}
+ const arr = [1, 2, 3, 4, 5];
+const checkNum = numberChecker(arr);
+
+console.log(checkNum(3)); // true
+console.log(checkNum(6)); // false
